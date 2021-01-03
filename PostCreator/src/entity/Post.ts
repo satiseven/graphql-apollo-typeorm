00443e234355b16baf41b03d8bf4693cb0055c97
Riza <satiseven777@gmail.com>
2021-01-03 19:45:25 +0300

    created entityandmigrations

---
+++ PostCreator/src/entity/Post.ts
@@ -0,0 +1,42 @@
+import "reflect-metadata";
+
+import { Field, Int, ObjectType } from "type-graphql";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { User } from "./User";
+@ObjectType()
+@Entity({ name: "posts" })
+export class Post {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn({ type: "int", unsigned: true })
+  id: number;
+  @Field(() => Int)
+  @Column({ type: "int", unsigned: true })
+  userId: number;
+  @Field(() => String)
+  @Column({ type: "char", length: 150 })
+  title: string;
+  @Field(() => String)
+  @Column({ type: "char", length: 150 })
+  slug: string;
+  @Field(() => String)
+  @Column({ type: "text" })
+  content: string;
+  @Field(() => String)
+  @Column({ type: "char", length: 150 })
+  image: string;
+  @Field(() => Date)
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  createdAt: Date;
+  @Field(() => Date)
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  updatedAt: Date;
+  @ManyToOne(() => User, (user) => user)
+  user: User;
+}
---
+++ PostCreator/src/entity/User.ts
@@ -0,0 +1,38 @@
+import "reflect-metadata";
+
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from "typeorm";
+import { Int, Field, ObjectType } from "type-graphql";
+import { Post } from "./Post";
+@ObjectType()
+@Entity({ name: "users" })
+export class User extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn({ type: "int", unsigned: true })
+  id: number;
+  @Field(() => String)
+  @Column({ type: "char", length: 150 })
+  username: string;
+  @Field(() => String)
+  @Column({ type: "char", length: 150 })
+  email: string;
+  @Field(() => String)
+  @Column({ type: "char", length: 150 })
+  password: string;
+  @Field(() => Date)
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  createdAt: Date;
+  @Field(() => Date)
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  updatedAt: Date;
+  @Field(() => [Post])
+  @OneToMany(() => Post, (post) => post.user)
+  posts: [Post];
+}
