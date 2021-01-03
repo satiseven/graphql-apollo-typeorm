f0844911192758d32f58f1ec154f40133d40dc7e
Riza <satiseven777@gmail.com>
2021-01-03 20:12:57 +0300

    Apollo server running

--- PostCreator/src/index.ts
+++ PostCreator/src/index.ts
@@ -1,1 +1,23 @@
 import "reflect-metadata";
+import express from "express";
+import { config } from "dotenv";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers/UserResolver";
+(async () => {
+  const PORT = process.env.PORT || 5000;
+  const app = express();
+  const graphqlServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [UserResolver] }),
+  });
+  graphqlServer.applyMiddleware({ app });
+  config({ path: ".env" });
+  app.get("/", (req, res) => {
+    res.end(
+      '<a href="http://localhost:5000/graphql" >You can Go to GraphQL Server</a>'
+    );
+  });
+  app.listen(PORT, () => {
+    console.log(`http://loclahost/${PORT}`);
+  });
+})();
